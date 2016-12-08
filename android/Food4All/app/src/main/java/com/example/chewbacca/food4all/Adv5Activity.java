b42723844e31af086911100b8a263ad212db1d23
jaycJulien <julien.christen@students.fhnw.ch>
2016-12-08 15:47:31 +0100

    add current camera functionality

--- android/Food4All/app/src/main/java/com/example/chewbacca/food4all/Adv5Activity.java
+++ android/Food4All/app/src/main/java/com/example/chewbacca/food4all/Adv5Activity.java
@@ -1,21 +1,42 @@
 package com.example.chewbacca.food4all;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
 
-public class Adv5Activity extends AppCompatActivity {
+
+public class Adv5Activity extends Activity {
+    private Camera mCamera = null;
+    private CameraView mCameraView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adv5);
-    }
 
-    /** Called when the user clicks the Login button */
-    public void goToAdv4Activity(View view) {
-        Intent intent = new Intent(this, Adv4Activity.class);
-        startActivity(intent);
+        try{
+            mCamera = Camera.open();//you can use open(int) to use different cameras
+        } catch (Exception e){
+            Log.d("ERROR", "Failed to get camera: " + e.getMessage());
+        }
+
+        if(mCamera != null) {
+            mCameraView = new CameraView(this, mCamera);//create a SurfaceView to show camera data
+            FrameLayout camera_view = (FrameLayout)findViewById(R.id.camera_view);
+            camera_view.addView(mCameraView);//add the SurfaceView to the layout
+        }
+
+        //btn to close the application
+        ImageButton imgClose = (ImageButton)findViewById(R.id.imgClose);
+        imgClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.exit(0);
+            }
+        });
     }
 }
