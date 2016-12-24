7f1c7316012084b9ede76fba8f40a8f1f2c7ee4b
Björn Stark <bjoern.stark@students.fhnw.ch>
2016-12-24 12:17:01 +0100

    Changed the RelativeLayout to LinearLayout in register4.xml and adjusted some general code for better readability

--- android/Food4All/app/src/main/java/com/example/chewbacca/food4all/Register4Activity.java
+++ android/Food4All/app/src/main/java/com/example/chewbacca/food4all/Register4Activity.java
@@ -1,7 +1,9 @@
 package com.example.chewbacca.food4all;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Register4Activity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Register4Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register4);
+    }
+
+    /** Called when the user clicks the Weiter-button */
+    public void goToStartscreen(View view) {
+        Intent intent = new Intent(this, Startscreen.class);
+        startActivity(intent);
     }
 }
