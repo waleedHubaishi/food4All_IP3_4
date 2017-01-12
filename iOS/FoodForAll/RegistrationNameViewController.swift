bfe337322e33efe9123e6fbba6c56154c085e9d1
Jalil Hashemi <jalil.hashemi@students.fhnw.ch>
2017-01-12 17:20:04 +0100

    Registration name characters are controlled and checked

--- iOS/FoodForAll/RegistrationNameViewController.swift
+++ iOS/FoodForAll/RegistrationNameViewController.swift
@@ -21,6 +21,45 @@ class RegistrationNameViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    @IBAction func toPasswordVC(){
+    checkUserName()
+    
+    }
+    
+    func displayAlertMessage(messageToDisplay: String)
+    {
+        print(messageToDisplay)
+        let alertController = UIAlertController(title: "Alert", message: messageToDisplay, preferredStyle: .alert)
+        
+        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
+            
+            // Code in this block will trigger when OK button tapped.
+            print("Ok button tapped");
+        }
+        
+        alertController.addAction(OKAction)
+        
+        self.present(alertController, animated: true, completion:nil)
+    }
+    
+    func checkUserName()
+    {
+    
+        let length = userNameTF.text?.characters.count
+        
+        if(length! < 20 && length! > 5)
+        {
+            let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "passWord") as! RegistrationPasswordViewController
+            self.navigationController?.pushViewController(secondViewController, animated: true)
+
+        }
+        
+        else
+        {
+          displayAlertMessage(messageToDisplay: "Emailadresse ist ungültig!")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
