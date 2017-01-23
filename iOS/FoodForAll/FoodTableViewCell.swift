a6c87f2bfb8fb3ee82125cc4b46115fd44c8fcf2
Waleed Al-Hubaishi <waleed.alhubaishi@students.fhnw.ch>
2017-01-23 20:56:03 +0100

    the FoodTableCell class was created and the branch iOS_search initialised

---
+++ iOS/FoodForAll/FoodTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FoodTableViewCell.swift
+//  FoodForAll
+//
+//  Created by Waleed al-Hubaishi on 23.01.17.
+//  Copyright © 2017 Waleed al-Hubaishi. All rights reserved.
+//
+
+import UIKit
+
+class FoodTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
