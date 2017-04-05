e0f4459342cf789b5e2082c5a77b1e238f7d9874
Waleed al-Hubaishi <waleed.alhubaishi@students.fhnw.ch>
2017-04-05 23:38:40 +0200

    connection between add food and search for food is established

--- iOS/FoodForAll/SearchHomeViewController.swift
+++ iOS/FoodForAll/SearchHomeViewController.swift
@@ -8,24 +8,42 @@
 
 import UIKit
 
-class SearchHomeViewController: UIViewController {
+class SearchHomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
      var foodList = [Food]()
+     let temp = Food();
     
+    @IBOutlet weak var foodTableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.foodTableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        // Do any additional setup after loading the view.
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         if(foodList.isEmpty)
         {
             print("empty")
+            
+            temp.name = "Pasta";
+            temp.category = "cooked";
+            temp.description = "delievered hot"
+            temp.expiration = "12.12.2017"
+            temp.pickedUp = "13.05.2017"
+            temp.pickedUpAt = "13:13"
+            temp.place = "Brugg"
+            
+            foodList.append(temp)
+            
         }
-        
+            
         else
         {
             print(foodList)
-            
         }
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -33,12 +51,24 @@ class SearchHomeViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return foodList.count
+    }
+    
      func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "LabelCell", for: indexPath) as! FoodTableViewCell
+    
         
-        let foodName = foodList[indexPath.row]
-        cell.foodName.text = foodName.name
-        
+        let foodInTable = foodList[indexPath.row]
+        cell.foodName.text = foodInTable.name
+        cell.foodType.text = foodInTable.category
+        cell.pickUpTillHour.text = foodInTable.pickedUpAt
+        cell.bisLbl.text = foodInTable.pickedUp
         
         return cell
     }
