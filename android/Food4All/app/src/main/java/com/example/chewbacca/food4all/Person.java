a882336c68ac2ef57d6b1965e79b3f9a8504c365
Björn Stark <bjoern.stark@students.fhnw.ch>
2017-04-29 17:22:59 +0200

    add Class Person was added with its class members, setters and getters

---
+++ android/Food4All/app/src/main/java/com/example/chewbacca/food4all/Person.java
@@ -0,0 +1,59 @@
+package com.example.chewbacca.food4all;
+
+/**
+ * This class is for controlling the user account but also
+ * for showing information of other users
+ */
+
+public class Person {
+    private String firstName,lastName, address, email;
+    private int rating, personID;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public int getPersonID() {
+        return personID;
+    }
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+}
