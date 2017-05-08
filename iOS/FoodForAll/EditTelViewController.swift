ac3ada6bbfc4fd406f1e4b11ccc318f354f7146d
Digitalninja <alessandro.calcagno@students.fhnw.ch>
2017-05-08 12:03:54 +0200

    ConfirmTel  erstellt

---
+++ iOS/FoodForAll/EditTelViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EditTelViewController.swift
+//  FoodForAll
+//
+//  Created by Alessandro Calcagno on 08.05.17.
+//  Copyright © 2017 Waleed al-Hubaishi. All rights reserved.
+//
+
+import UIKit
+
+class EditTelViewController: UIViewController {
+
+    
+    @IBOutlet var NameLabel: UILabel!
+    
+    @IBOutlet var Profilepicture: UIImageView!
+    
+    @IBOutlet var RankLabel: UILabel!
+    
+    @IBOutlet var ConfirmNewTel: UITextField!
+    
+    @IBOutlet var AbortButton: UIButton!
+    
+    @IBOutlet var ConfirmButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
