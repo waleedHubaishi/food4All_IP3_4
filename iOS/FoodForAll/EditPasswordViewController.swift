511aebb134f8fb8142ebea9b38aee222da4ac384
Digitalninja <alessandro.calcagno@students.fhnw.ch>
2017-05-08 10:54:51 +0200

    Edit Password classes

---
+++ iOS/FoodForAll/EditPasswordViewController.swift
@@ -0,0 +1,53 @@
+//
+//  EditPasswordViewController.swift
+//  FoodForAll
+//
+//  Created by Alessandro Calcagno on 08.05.17.
+//  Copyright © 2017 Waleed al-Hubaishi. All rights reserved.
+//
+
+import UIKit
+
+class EditPasswordViewController: UIViewController {
+
+    
+    @IBOutlet var NameLabel: UILabel!
+    
+    @IBOutlet var Profilepicture: UIImageView!
+    
+    @IBOutlet var RankLabel: UILabel!
+    
+    @IBOutlet var OldPassword: UITextField!
+    
+    @IBOutlet var NewPassword: UITextField!
+    
+    @IBOutlet var ConfirmNewPassword: UITextField!
+    
+    @IBOutlet var CommitButton: UIButton!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
