e7fc071f4b553c446b7ac1e14591ba645b44f7c0
Hussein Farzi <hussein.farzi@students.fhnw.ch>
2017-05-09 23:39:07 +0200

    Add new class EmailExistenceViewController to check email existence in DB

--- iOS/FoodForAll/RegistrationNameViewController.swift
+++ iOS/FoodForAll/RegistrationNameViewController.swift
@@ -21,37 +21,34 @@ class RegistrationNameViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
     
-    // 
-    
-    func isInputValid(inputLength:Int) -> Bool
-    {
+    func isInputValid(inputLength:Int) -> Bool {
         if((inputLength < 5) || (inputLength > 20))
         {
-            
             return false
         }
-        
         return true
     }
     
     @IBAction func toPasswordVC(){
         
         let length = userNameTF.text?.characters.count
-//        checkingEmailFT(emailTF: emailTF.text!)
-        if (isInputValid(inputLength: length!) == false){
+
+        if (isInputValid(inputLength: length!) == false) {
             displayAlertMessage(messageToDisplay: "Benutzername ist ungültig!")
         }
-        
-        if !(EmailTextFieldVerficationViewController().isValidEmailAddress(emailAddressString: emailTF.text!)){
-            displayAlertMessage(messageToDisplay: "Emailadresse ist ungültig!")}
-            
-        else{
+        if !(EmailTextFieldVerficationViewController().isValidEmailAddress(emailAddressString: emailTF.text!)) {
+            displayAlertMessage(messageToDisplay: "Emailadresse ist ungültig!")
+        }
+        if !(EmailExistenceViewController().existEmailAddress(emailAddressString: emailTF.text!)) {
+            displayAlertMessage(messageToDisplay: "Emailadresse existiert!")
+        }
+        else {
             let secondViewController = self.storyboard?.instantiateViewController(withIdentifier: "passWord") as! RegistrationPasswordViewController
-            self.navigationController?.pushViewController(secondViewController, animated: true)}
+            self.navigationController?.pushViewController(secondViewController, animated: true)
+        }
     }
     
-    func displayAlertMessage(messageToDisplay: String)
-    {
+    func displayAlertMessage(messageToDisplay: String) {
         print(messageToDisplay)
         let alertController = UIAlertController(title: "Fehler", message: messageToDisplay, preferredStyle: .alert)
         
@@ -60,18 +57,9 @@ class RegistrationNameViewController: UIViewController {
             // Code in this block will trigger when OK button tapped.
             print("Ok button tapped");
         }
-        
         alertController.addAction(OKAction)
-        
         self.present(alertController, animated: true, completion:nil)
     }
-    
-//    func checkingEmailFT(emailTF : String){
-//        EmailTextFieldVerficationViewController().emailVerification(emailT: emailTF)
-//        if EmailTextFieldVerficationViewController().isValidEmailAddress(emailAddressString: emailTF) == false {
-//            displayAlertMessage(messageToDisplay: "Emailadresse ist ungültig!")
-//        }
-//    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
