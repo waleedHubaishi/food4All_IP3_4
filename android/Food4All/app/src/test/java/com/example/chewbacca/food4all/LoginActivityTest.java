1790acb4f5cc0e4d8165658149f10a792729383a
Manuel Hunziker <manuel.hunziker1@students.fhnw.ch>
2017-05-09 15:59:45 +0200

    add profile activity

---
+++ android/Food4All/app/src/test/java/com/example/chewbacca/food4all/LoginActivityTest.java
@@ -0,0 +1,38 @@
+package com.example.chewbacca.food4all;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by xao on 01.05.2017.
+ */
+public class LoginActivityTest {
+    @Test
+    public void onRequestPermissionsResult() throws Exception {
+
+        // given
+        int grantResults = 0;
+
+        // when
+
+        // then
+
+    }
+
+    @Test
+    public void onCreateLoader() throws Exception {
+
+    }
+
+    @Test
+    public void onLoadFinished() throws Exception {
+
+    }
+
+    @Test
+    public void onLoaderReset() throws Exception {
+
+    }
+
+}
