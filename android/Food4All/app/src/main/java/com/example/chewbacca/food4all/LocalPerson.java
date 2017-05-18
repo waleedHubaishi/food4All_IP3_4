9bd60561c002e48155a9a7fb4f366beb4a3c48da
Björn Stark <bjoern.stark@students.fhnw.ch>
2017-05-18 14:04:22 +0200

    add Added LocalPerson (inherits from Person) which represents the logged in user

---
+++ android/Food4All/app/src/main/java/com/example/chewbacca/food4all/LocalPerson.java
@@ -0,0 +1,43 @@
+package com.example.chewbacca.food4all;
+
+import java.util.List;
+
+/**
+ * This Class represents the Person who logged in locally
+ */
+
+public class LocalPerson extends Person {
+    private List<Advertisement> advertisements;
+
+    public void editEmail(String email) {
+
+    }
+
+    public void editPassword(String pass) {
+
+    }
+
+    public void deleteUser() {
+
+    }
+
+    public void addAdv(Advertisement adv) {
+
+    }
+
+    public void editAdv(Advertisement adv) {
+
+    }
+
+    public List<Advertisement> getAdvertisements() {
+        return advertisements;
+    }
+
+    public void reserveAdv(Advertisement adv) {
+
+    }
+
+    public void ratePerson(Person pers) {
+
+    }
+}
