9a8bdbec91ff235c7203c06f27cce90a0534d91a
Waleed al-Hubaishi <waleed.alhubaishi@students.fhnw.ch>
2017-05-31 14:02:37 +0200

    reserviert and abholen text size and color is adjusted

--- iOS/FoodForAll/ChosenFoodViewController.swift
+++ iOS/FoodForAll/ChosenFoodViewController.swift
@@ -89,6 +89,27 @@ class ChosenFoodViewController: UIViewController {
     }
     
     
+    override func viewWillAppear(_ animated: Bool) {
+        //defines the header colors
+        let nav = self.navigationController?.navigationBar
+        
+        //changes the navigation bar color
+        self.navigationController?.navigationBar.barTintColor = UIColor.init(red: 70.0/255.0, green: 171.0/255.0, blue: 215.0/255.0, alpha: 1.0)
+        
+        //changes the icon color
+        nav?.tintColor = UIColor.white
+        
+        //changes the title color
+        nav?.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.white]
+        
+        //add myFridge to the navigation bar
+        
+        let button = UIButton.init(type: .custom)
+        button.setImage(UIImage.init(named: "myfridge.png"), for: UIControlState.normal)
+        button.frame = CGRect.init(x: 0, y: 0, width: 65, height: 40)
+        let barButton = UIBarButtonItem.init(customView: button)
+        self.navigationItem.rightBarButtonItem = barButton
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
